90e06e52978becb3b8e2407491564cce14931d67
jbberlioz <jbberlioz@f53e1053-3c5d-0410-8e53-a874684037d5>
2008-12-05 13:53:12 +0000

    [master][tostlib] configuration manager draft (not tested)

---
+++ branches/devel/src/master/tostlib/configuration.py
@@ -0,0 +1,380 @@
+#==========================================================================
+# tostdk :: tostlib :: configuration.py
+# Configuration manager
+#--------------------------------------------------------------------------
+# Copyright 2009 Jean-Baptiste Berlioz
+#--------------------------------------------------------------------------
+# This file is part of Tostdk.
+#
+# Tostdk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tostdk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tostdk.  If not, see <http://www.gnu.org/licenses/>.
+#==========================================================================
+
+
+import os
+
+from singleton import Singleton
+
+
+#==========================================================================
+SITE_CONFIG_FILENAMES   = ('/etc/tostdk',)		# absolute path
+USER_CONFIG_FILENAMES   = ('~/.tostdk',)		# absolute path
+PROJECT_CONFIG_FILENAME = '.tostdk/config'		# relative path
+#--------------------------------------------------------------------------
+LEVEL_SITE    = 0	# option is defined in level configuration
+LEVEL_USER    = 1	# option is definied in user configuration
+LEVEL_PROJECT = 2	# option is defined in project configuration
+#==========================================================================
+
+
+#==========================================================================
+TYPE_BOOL = {
+	'desc' : "Boolean",
+	'enum' : ('false', 'true'),
+	'valid': lambda x: x.lower() in ('false', 'true'),
+	'read' : lambda x: x.lower() == 'true',
+	'write': lambda x: ['false','true'][bool(x)]
+}
+#==========================================================================
+
+
+#==========================================================================
+CONFIGURATION = {
+#==========================================================================
+
+	#----------------------------------------------------------------------
+	'general': {
+	#----------------------------------------------------------------------
+
+		#------------------------------------------------------------------
+		'debug': {
+		#------------------------------------------------------------------
+			'level'  : LEVEL_USER,
+			'desc'   : "Debug mode",
+			'type'   : TYPE_BOOL,
+			'default': False
+		},
+
+		#------------------------------------------------------------------
+		'verbose': {
+		#------------------------------------------------------------------
+			'level'  : LEVEL_USER,
+			'desc'   : "Verbose mode",
+			'type'   : TYPE_BOOL,
+			'default': False
+		}
+	}
+}
+#==========================================================================
+
+
+#==========================================================================
+class Configuration ( Singleton ):
+#==========================================================================
+
+	s_instance = None
+
+	#----------------------------------------------------------------------
+	def __init__ ( self ):
+	#----------------------------------------------------------------------
+
+		Singleton.__init__(self)
+
+		self.m_site_filename = self.__find_site_configuration()
+		self.m_user_filename = self.__find_user_configuration()
+
+		self.m_sections = {}
+
+	#----------------------------------------------------------------------
+	def load ( self, p_project_path = '' ):
+	#----------------------------------------------------------------------
+
+		self.m_sections = {}
+
+		if self.m_site_filename:
+			self.__load_configuration(LEVEL_SITE, self.m_site_filename)
+
+		if self.m_user_filename:
+			self.__load_configuration(LEVEL_USER, self.m_user_filename)
+
+		if p_project_path:
+			l_filename = self.__find_project_configuration(p_project_path)
+			if os.path.exists(l_filename):
+				self.__load_configuration(LEVEL_PROJECT, l_filename)
+
+	#----------------------------------------------------------------------
+	def save ( self, p_project_path = '' ):
+	#----------------------------------------------------------------------
+
+		if self.m_user_filename:
+			self.__save_configuration(LEVEL_USER, self.m_user_filename)
+
+		if p_project_path:
+			l_filename = self.__find_project_configuration(p_project_path)
+			self.__save_configuration(LEVEL_PROJECT, l_filename)
+
+	#----------------------------------------------------------------------
+	def sections_iterator ( self ):
+	#----------------------------------------------------------------------
+
+		return CONFIGURATION.iterkeys()
+
+	#----------------------------------------------------------------------
+	def has_section ( self, p_section ):
+	#----------------------------------------------------------------------
+
+		return CONFIGURATION.has_key(p_section.lower())
+
+	#----------------------------------------------------------------------
+	def options_iterator ( self, p_section ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+
+		if CONFIGURATION.has_key(l_section):
+			return CONFIGURATION[l_section].iterkeys()
+		return None
+
+	#----------------------------------------------------------------------
+	def has_option ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		return CONFIGURATION.has_key(l_section) and \
+			   CONFIGURATION[l_section].has_key(l_option)
+
+	#----------------------------------------------------------------------
+	def get_option_value ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if self.m_sections.has_key(l_section) and \
+		   self.m_sections[l_section].has_key(l_option):
+		   	return self.m_sections[l_section][l_option]['value']
+
+		return self.get_option_default(l_section, l_option)
+
+	#----------------------------------------------------------------------
+	def get_option_default ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if CONFIGURATION.has_key(l_section) and \
+		   CONFIGURATION[l_section].has_key(l_option):
+			return CONFIGURATION[l_section][l_option]['default']
+		return None
+
+	#----------------------------------------------------------------------
+	def get_option_desc ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if CONFIGURATION.has_key(l_section) and \
+		   CONFIGURATION[l_section].has_key(l_option):
+			if CONFIGURATION[l_section][l_option].has_key('desc'):
+				return CONFIGURATION[l_section][l_option]['desc']
+			return ''
+		return None
+
+	#----------------------------------------------------------------------
+	def get_option_enum ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if CONFIGURATION.has_key(l_section) and \
+		   CONFIGURATION[l_section].has_key(l_option):
+			if CONFIGURATION[l_section][l_option].has_key('enum'):
+				return CONFIGURATION[l_section][l_option]['enum']
+			return tuple()
+		return None
+
+	#----------------------------------------------------------------------
+	def get_option_level ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if self.m_sections.has_key(l_section) and \
+		   self.m_sections[l_section].has_key(l_option):
+			return self.m_sections[l_section][l_option]['level']
+
+		return self.get_option_default_level(p_section, p_option)
+
+	#----------------------------------------------------------------------
+	def get_option_default_level ( self, p_section, p_option ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if CONFIGURATION.has_key(l_section) and \
+		   CONFIGURATION[l_section].has_key(l_option):
+			return CONFIGURATION[l_section][l_option]['level']
+		return None
+
+	#----------------------------------------------------------------------
+	def set_option_value ( self, p_level, p_section, p_option, p_value ):
+	#----------------------------------------------------------------------
+
+		l_section = p_section.lower()
+		l_option  = p_option.lower()
+
+		if CONFIGURATION.has_key(l_section) and \
+		   CONFIGURATION[l_section].has_key(l_option):
+			if CONFIGURATION[l_section][l_option]['level'] >= p_level:
+				if not self.m_sections.has_key(l_section):
+					self.m_sections[l_section] = {}
+				self.m_sections[l_section][l_option] = {'level':p_level,'value':p_value}
+				return True
+		return False
+
+	#----------------------------------------------------------------------
+	def __find_site_configuration ( self ):
+	#----------------------------------------------------------------------
+
+		for l_filename in SITE_CONFIG_FILENAMES:
+			if os.path.exists(l_filename):
+				return l_filename
+
+		return ''
+
+	#----------------------------------------------------------------------
+	def __find_user_configuration ( self ):
+	#----------------------------------------------------------------------
+
+		for l_filename in USER_CONFIG_FILENAMES:
+			l_user_filename = os.path.expanduser(l_filename)
+			if os.path.exists(l_user_filename):
+				return l_user_filename
+
+		return ''
+
+	#----------------------------------------------------------------------
+	def __find_project_configuration ( self, p_project_path ):
+	#----------------------------------------------------------------------
+
+		l_filename = os.path.join(p_project_path, PROJECT_CONFIG_FILENAME)
+		return l_filename
+
+	#----------------------------------------------------------------------
+	def __load_configuration ( self, p_level, p_filename ):
+	#----------------------------------------------------------------------
+
+		l_handle = open(p_filename, 'rU')
+
+		l_section = ''
+		l_skip    = False
+
+		for l_raw_line in l_handle:
+			l_line = l_raw_line.strip().lower()
+
+			if not l_line:
+				continue
+
+			if l_line[0] == '#':
+				continue
+
+			if '[' in l_line:
+				l_beg = l_line.find('[')
+				l_end = l_line.rfind(']')
+
+				if l_beg == -1 or l_end == -1:
+					l_skip = True
+					continue
+
+				l_section = l_line[l_beg+1:l_end].lower()
+
+				if not l_section:
+					l_skip = True
+					continue
+
+				if not CONFIGURATION.has_key(l_section):
+					l_skip = True
+					continue
+
+				l_skip = False
+
+				if not self.m_sections.has_key(l_section):
+					self.m_sections[l_section] = {}
+
+			elif not l_skip:
+				l_split = map(lambda x: x.strip().lower(), l_line.split('='))
+
+				if len(l_split) != 2:
+					continue
+
+				l_option, l_string = l_split
+
+				if not CONFIGURATION[l_section].has_key(l_option):
+					continue
+
+				if CONFIGURATION[l_section][l_option]['level'] < p_level:
+					continue
+
+				l_type = CONFIGURATION[l_section][l_option]['type']
+
+				if not l_type['valid'](l_string):
+					continue
+
+				l_value = l_type['read'](l_string)
+
+				self.m_sections[l_section][l_option] = {'level':p_level,'value':l_value}
+
+		l_handle.close()
+
+	#----------------------------------------------------------------------
+	def __save_configuration ( self, p_level, p_filename ):
+	#----------------------------------------------------------------------
+
+		l_handle = open(p_filename, 'wb')
+
+		for l_section in self.m_sections.iterkeys():
+			l_section_started = False
+
+			for l_option in l_section.iterkeys():
+
+				if self.m_sections[l_section][l_option]['level'] != p_level:
+					continue
+
+				if not l_section_started:
+					l_handle.write('[' + l_section + ']' + os.linesep)
+					l_section_started = True
+
+				if CONFIGURATION[l_section][l_option].has_key('desc'):
+					l_desc = CONFIGURATION[l_section][l_option]['desc']
+					l_handle.write('# ' + l_desc + os.linesep)
+
+				l_type   = CONFIGURATION[l_section][l_option]['type']
+				l_string = l_type['write'](self.m_sections[l_section][l_option]['value'])
+				l_hande.write(l_option + ' = ' + l_string + os.linesep)
+			
+			if l_section_started:
+				l_handle.write(os.linesep)
+
+		l_handle.close()
+
+
+#==========================================================================
+# End
+#==========================================================================
