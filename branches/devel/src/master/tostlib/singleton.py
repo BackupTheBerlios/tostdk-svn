248f21f1ababeda741284bc6d7ba0d60f20849d4
jbberlioz <jbberlioz@f53e1053-3c5d-0410-8e53-a874684037d5>
2008-12-05 13:45:44 +0000

    [master][tostlib] add singleton class

---
+++ branches/devel/src/master/tostlib/singleton.py
@@ -0,0 +1,57 @@
+#==========================================================================
+# tostdk :: tostlib :: singleton.py
+# Singleton base class
+#--------------------------------------------------------------------------
+# Copyright 2009 Jean-Baptiste Berlioz
+#--------------------------------------------------------------------------
+# This file is part of Tostdk.
+#
+# Tostdk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tostdk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tostdk.  If not, see <http://www.gnu.org/licenses/>.
+#==========================================================================
+
+
+#==========================================================================
+class Singleton:
+#==========================================================================
+
+	# NOTE: s_instance class attribute must be declared
+	#       in the derived class!
+
+	#----------------------------------------------------------------------
+	@classmethod
+	def get_instance ( cls ):
+	#----------------------------------------------------------------------
+
+		if cls.s_instance == None:
+			cls.s_instance = cls()
+
+		return cls.s_instance
+
+	#----------------------------------------------------------------------
+	@classmethod
+	def del_instance ( cls ):
+	#----------------------------------------------------------------------
+
+		cls.s_instance = None
+
+	#----------------------------------------------------------------------
+	def __init__ ( self ):
+	#----------------------------------------------------------------------
+
+		assert (self.s_instance == None)
+
+
+#==========================================================================
+# End
+#==========================================================================
