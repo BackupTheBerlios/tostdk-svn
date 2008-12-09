7f31184132fdeadcdfe78460b745d5d629fee8c8
jbberlioz <jbberlioz@f53e1053-3c5d-0410-8e53-a874684037d5>
2008-12-09 21:46:16 +0000

    [master][drivers] add files_driver

--- branches/devel/src/master/drivers/files_driver/files_driver.py
+++ branches/devel/src/master/drivers/files_driver/files_driver.py
@@ -1,6 +1,6 @@
 #==========================================================================
-# tostdk :: drivers :: driver.py
-# Base driver class
+# tostdk :: drivers :: files_driver.py
+# Test driver, using files for input / output
 #--------------------------------------------------------------------------
 # Copyright 2009 Jean-Baptiste Berlioz
 #--------------------------------------------------------------------------
@@ -20,45 +20,68 @@
 # along with Tostdk.  If not, see <http://www.gnu.org/licenses/>.
 #==========================================================================
 
+import os
 
-import tostlib.singleton as singleton
-
+import drivers.driver as driver
+import tostlib.configuration as configuration
 
 #==========================================================================
-class Driver ( singleton.Singleton ):
+class FilesDriver ( driver.Driver ):
 #==========================================================================
-
-	s_instance = None
 
 	#----------------------------------------------------------------------
 	def __init__ ( self ):
 	#----------------------------------------------------------------------
 
-		singleton.Singleton.__init__(self)
+		driver.Driver.__init__(self)
+
+		self.m_input_handle  = None
+		self.m_output_handle = None
 
 	#----------------------------------------------------------------------
 	def open ( self ):
 	#----------------------------------------------------------------------
 
-		raise NotImplementedError
+		l_config = configuration.Configuration.get_instance()
+
+		l_input_path  = l_config.get_option_value('drivers', 'files_driver.input')
+		l_output_path = l_config.get_option_value('drivers', 'files_driver.output')
+
+		if not l_input_path or not l_output_path:
+			return False
+
+		try:
+			open(l_input_path,  'wb').close()
+			open(l_output_path, 'wb').close()
+		except:
+			return False
+
+		self.m_input_handle  = open(l_input_path,  'rb', 0)
+		self.m_output_handle = open(l_output_path, 'wb', 0)
+
+		return True
 
 	#----------------------------------------------------------------------
 	def close ( self ):
 	#----------------------------------------------------------------------
 
-		raise NotImplementedError
+		if self.m_input_handle != None:
+			self.m_input_handle.close()
+
+		if self.m_output_handle != None:
+			self.m_output_handle.close()
 
 	#----------------------------------------------------------------------
 	def read ( self ):
 	#----------------------------------------------------------------------
 
-		raise NotImplementedError
+		return self.m_input_handle.read()
 
 	#----------------------------------------------------------------------
 	def write ( self, p_data ):
 	#----------------------------------------------------------------------
 
-		raise NotImplementedError
+		self.m_output_handle.write(p_data)
 
 
 #==========================================================================
