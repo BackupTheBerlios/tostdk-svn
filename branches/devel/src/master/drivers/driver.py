4763a895965fe8ab8d337ee582707292936cb87e
jbberlioz <jbberlioz@f53e1053-3c5d-0410-8e53-a874684037d5>
2008-12-09 20:49:12 +0000

    [master][drivers] add base driver class

---
+++ branches/devel/src/master/drivers/driver.py
@@ -0,0 +1,72 @@
+#==========================================================================
+# tostdk :: drivers :: driver.py
+# Base driver class
+#--------------------------------------------------------------------------
+# Copyright 2009 Jean-Baptiste Berlioz
+#--------------------------------------------------------------------------
+# This file is part of Tostdk.
+#
+# Tostdk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tostdk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tostdk.  If not, see <http://www.gnu.org/licenses/>.
+#==========================================================================
+
+
+import tostlib.singleton as singleton
+
+
+#==========================================================================
+class Driver ( singleton.Singleton ):
+#==========================================================================
+
+	s_instance = None
+
+	#----------------------------------------------------------------------
+	def __init__ ( self ):
+	#----------------------------------------------------------------------
+
+		singleton.Singleton.__init__(self)
+
+	#----------------------------------------------------------------------
+	def configure ( self ):
+	#----------------------------------------------------------------------
+
+		raise NotImplementedError
+
+	#----------------------------------------------------------------------
+	def open ( self ):
+	#----------------------------------------------------------------------
+
+		raise NotImplementedError
+
+	#----------------------------------------------------------------------
+	def close ( self ):
+	#----------------------------------------------------------------------
+
+		raise NotImplementedError
+
+	#----------------------------------------------------------------------
+	def read ( self ):
+	#----------------------------------------------------------------------
+
+		raise NotImplementedError
+
+	#----------------------------------------------------------------------
+	def write ( self, p_string ):
+	#----------------------------------------------------------------------
+
+		raise NotImplementedError
+
+
+#==========================================================================
+# End
+#==========================================================================
