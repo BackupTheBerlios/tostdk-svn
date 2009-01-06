d4e97c265a60f876b10e842822bb38b057dd4fb0
jbberlioz <jbberlioz@f53e1053-3c5d-0410-8e53-a874684037d5>
2009-01-06 15:15:03 +0000

    [master][tostlib] add configuration options for packet data size and commands timeouts

--- branches/devel/src/master/tostlib/master.py
+++ branches/devel/src/master/tostlib/master.py
@@ -21,44 +21,38 @@
 #==========================================================================
 
 
+import configuration
 import pooky
 import opcodes
 import command
 
 
 #==========================================================================
-PACKET_MAX_DATA_SIZE = 4096
-#--------------------------------------------------------------------------
-PING_TIMEOUT		= 1.0	# FIXME: ugly harcoded timeouts
-CHROOT_TIMEOUT		= 10.0
-MV_TIMEOUT			= 10.0
-RM_TIMEOUT			= 10.0
-MALLOC_TIMEOUT		= 10.0
-FREE_TIMEOUT		= 10.0
-MEMMOVE_TIMEOUT		= 10.0
-DOWNLOAD_TIMEOUT	= 60.0
-UNPACK_TIMEOUT		= 60.0
-OPEN_TIMEOUT		= 10.0
-CREATE_TIMEOUT		= 10.0
-SEEK_TIMEOUT		= 10.0
-READ_TIMEOUT		= 60.0
-WRITE_TIMEOUT		= 60.0
-CLOSE_TIMEOUT		= 10.0
-#==========================================================================
-
-
-#==========================================================================
 def make_project_commands ( p_slavepath ):
 #==========================================================================
 
+	l_config = configuration.Configuration.get_instance()
+	l_ping_timeout   = l_config.get_option_value('protocol', 'ping_timeout')
+	l_chroot_timeout = l_config.get_option_value('protocol', 'chroot_timeout')
+
 	return [
-		command.Command.create(opcodes.PING,   [],            PING_TIMEOUT),
-		command.Command.Create(opcodes.CHROOT, [p_slavepath], CHROOT_TIMEOUT)
+		command.Command.create(opcodes.PING,   [],            l_ping_timeout),
+		command.Command.Create(opcodes.CHROOT, [p_slavepath], l_chroot_timeout)
 	]
 
 #==========================================================================
 def make_add_commands ( p_guid, p_filename, p_filedata ):
 #==========================================================================
+
+	l_config = configuration.Configuration.get_instance()
+	l_max_data_size    = l_config.get_option_value('protocol', 'packet_size')
+	l_malloc_timeout   = l_config.get_option_value('protocol', 'malloc_timeout')
+	l_download_timeout = l_config.get_option_value('protocol', 'download_timeout')
+	l_unpack_timeout   = l_config.get_option_value('protocol', 'unpack_timeout')
+	l_create_timeout   = l_config.get_option_value('protocol', 'create_timeout')
+	l_write_timeout    = l_config.get_option_value('protocol', 'write_timeout')
+	l_close_timeout    = l_config.get_option_value('protocol', 'close_timeout')
+	l_free_timeout     = l_config.get_option_value('protocol', 'free_timeout')
 
 	l_file_size = len(p_filedata)
 	l_data      = pooky.pack(p_filedata)
@@ -66,28 +60,28 @@ def make_add_commands ( p_guid, p_filename, p_filedata ):
 	l_offset    = l_file_size - l_data_size
 
 	l_seq  = [
-		command.Command.create(opcodes.MALLOC, [l_file_size], MALLOC_TIMEOUT)
+		command.Command.create(opcodes.MALLOC, [l_file_size], l_malloc_timeout)
 	]
 
 	l_current = l_offset
 
 	while l_data:
-		l_packet      = l_data[:PACKET_MAX_DATA_SIZE]
+		l_packet      = l_data[:l_max_data_size]
 		l_packet_size = len(l_packet)
 		l_data        = l_data[l_packet_size:]
 
 		l_seq.append(
 			command.Command.create(opcodes.DOWNLOAD,
-				[l_current, l_packet_size, l_packet], DOWNLOAD_TIMEOUT))
+				[l_current, l_packet_size, l_packet], l_download_timeout))
 
 		l_current += l_packet_size
 
 	l_seq.extend([
-		command.Command.create(opcodes.UNPACK, [l_offset, 0],    UNPACK_TIMEOUT),
-		command.Command.create(opcodes.CREATE, [p_filename],     CREATE_TIMEOUT),
-		command.Command.create(opcodes.WRITE,  [0, l_file_size], WRITE_TIMEOUT),
-		command.Command.create(opcodes.CLOSE,  [],               CLOSE_TIMEOUT),
-		command.Command.create(opcodes.FREE,   [],               FREE_TIMEOUT, p_guid)
+		command.Command.create(opcodes.UNPACK, [l_offset, 0],    l_unpack_timeout),
+		command.Command.create(opcodes.CREATE, [p_filename],     l_create_timeout),
+		command.Command.create(opcodes.WRITE,  [0, l_file_size], l_write_timeout),
+		command.Command.create(opcodes.CLOSE,  [],               l_close_timeout),
+		command.Command.create(opcodes.FREE,   [],               l_free_timeout, p_guid)
 	])
 
 	return l_seq
@@ -96,39 +90,58 @@ def make_add_commands ( p_guid, p_filename, p_filedata ):
 def make_remove_commands ( p_guid, p_filename ):
 #==========================================================================
 
+	l_config = configuration.Configuration.get_instance()
+	l_rm_timeout = l_config.get_option_value('protocol', 'rm_timeout')
+
 	return [
-		command.Command.create(opcodes.RM, [p_filename], RM_TIMEOUT, p_guid)
+		command.Command.create(opcodes.RM, [p_filename], l_rm_timeout, p_guid)
 	]
 
 #==========================================================================
 def make_rename_commands ( p_guid, p_old_filename, p_new_filename ):
 #==========================================================================
 
+	l_config = configuration.Configuration.get_instance()
+	l_mv_timeout = l_config.get_option_value('protocol', 'mv_timeout')
+
 	return [
 		command.Command.create(opcodes.MV,
 			[p_old_filename, p_new_filename],
-			RM_TIMEOUT, p_guid)
+			l_mv_timeout, p_guid)
 	]
 
 #==========================================================================
 def make_update_commands ( p_guid, p_filename, p_diff ):
 #==========================================================================
 
+	l_config = configuration.Configuration.get_instance()
+	l_max_data_size    = l_config.get_option_value('protocol', 'packet_size')
+	l_malloc_timeout   = l_config.get_option_value('protocol', 'malloc_timeout')
+	l_download_timeout = l_config.get_option_value('protocol', 'download_timeout')
+	l_unpack_timeout   = l_config.get_option_value('protocol', 'unpack_timeout')
+	l_open_timeout     = l_config.get_optiob_value('protocol', 'open_timeout')
+	l_create_timeout   = l_config.get_option_value('protocol', 'create_timeout')
+	l_seek_timeout     = l_config.get_option_value('protocol', 'seek_timeout')
+	l_read_timeout     = l_config.get_option_value('protocol', 'read_timeout')
+	l_write_timeout    = l_config.get_option_value('protocol', 'write_timeout')
+	l_close_timeout    = l_config.get_option_value('protocol', 'close_timeout')
+	l_free_timeout     = l_config.get_option_value('protocol', 'free_timeout')
+
 	l_orig_size, l_final_size, l_matcher = p_diff
 
 	l_seq = [
-		command.Command.create(opcodes.MALLOC, [l_final_size],   MALLOC_TIMEOUT),
-		command.Command.create(opcodes.OPEN,   [p_filename],     OPEN_TIMEOUT),
-		command.Command.create(opcodes.READ,   [0, l_orig_size], READ_TIMEOUT)
+		command.Command.create(opcodes.MALLOC, [l_final_size],   l_malloc_timeout),
+		command.Command.create(opcodes.OPEN,   [p_filename],     l_open_timeout),
+		command.Command.create(opcodes.READ,   [0, l_orig_size], l_read_timeout)
 	]
 
 	# TODO: read docs and implement...
 
 	l_seq.extend([
-		command.Command.create(opcodes.SEEK,  [0],               SEEK_TIMEOUT),
-		command.Command.create(opcodes.WRITE, [0, l_final_size], WRITE_TIMEOUT),
-		command.Command.create(opcodes.CLOSE, [],                CLOSE_TIMEOUT),
-		command.Command.create(opcodes.FREE,  [],                FREE_TIMEOUT, p_guid)
+		command.Command.create(opcodes.SEEK,  [0],               l_seek_timeout),
+		command.Command.create(opcodes.WRITE, [0, l_final_size], l_write_timeout),
+		command.Command.create(opcodes.CLOSE, [],                l_close_timeout),
+		command.Command.create(opcodes.FREE,  [],                l_free_timeout, p_guid)
 	])
 
 	return l_seq
